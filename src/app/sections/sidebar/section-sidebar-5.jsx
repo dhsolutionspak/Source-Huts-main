8f4ee7cfefbc1c8ce30092ea2d3c00c83402cad8
dhsolutionspak <digitalhousepakistan@gmail.com>
2025-02-09 01:14:23 +0500

    Sidebar Fix

--- src/app/sections/sidebar/section-sidebar-5.jsx
+++ src/app/sections/sidebar/section-sidebar-5.jsx
@@ -1,7 +1,7 @@
 import SectionSideContact from "./section-side-contact";
 import SectionSideGallery from "./section-side-gallery";
-import SectionSidePosts from "./section-side-posts";
-import SectionSideSearch from "./section-side-search";
+
+
 import SectionSideServices from "./section-side-services";
 
 import { data } from "../../../globals/data/sidebar";
@@ -9,9 +9,8 @@ import { data } from "../../../globals/data/sidebar";
 export default function SectionSidebar() {
     return (
         <>
-            <SectionSideSearch />
+ 
             <SectionSideServices _data={data.allservices} />
-            <SectionSidePosts _data={data.posts} />
             <SectionSideGallery _data={data.gallery} />
 
             <SectionSideContact />
